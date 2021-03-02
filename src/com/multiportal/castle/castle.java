19d92acc04d1304e005c949798b4b6f4140104f5
jakubiszon26 <itcppprograming@gmail.com>
2021-03-02 16:29:54 +0100

    polished enabled message

--- src/com/multiportal/castle/castle.java
+++ src/com/multiportal/castle/castle.java
@@ -8,11 +8,19 @@ public class castle extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new playerJoinGame(), this);
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "castle is enabled");
-
+        //WARING!!!
+        //If you won't credit us, you might get CONFLICT WITH LAW!!!
+        //For more info view GPL 3.0 license
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "  __  __       _ _   _ ____            _        _");
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + " |  \\/  |_   _| | |_(_)  _ \\ ___  _ __| |_ __ _| |");
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + " | |\\/| | | | | | __| | |_) / _ \\| '__| __/ _` | |");
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + " | |  | | |_| | | |_| |  __/ (_) | |  | || (_| | |");
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + " |_|  |_|\\__,_|_|\\__|_|_|   \\___/|_|   \\__\\__,_|_|");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Castle Plugin by MultiPortal is enabled");
     }
+
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.RED + "castle is disabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "Castle Plugin by MultiPortal is disabled");
     }
 }
